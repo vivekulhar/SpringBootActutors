5e626c6415a461504d71cc059a86c3449c8f5e43
Vivekkulhar <105244989+Vivekkulhar@users.noreply.github.com>
2023-11-04 14:43:44 +0530

    Tweak ith actuators

---
+++ src/main/java/dev/vivek/springbootactuatorapp/SpringBootActuatorAppApplication.java
@@ -0,0 +1,13 @@
+package dev.vivek.springbootactuatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuatorAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vivek/springbootactuatorapp/SpringBootActuatorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vivek.springbootactuatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
